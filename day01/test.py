71b17f6acce7992b89c53486e78f0eded9edef7b
sommaanny <a000626@naver.com>
2024-01-15 13:21:20 +0900

    day02

--- day01/test.py
+++ day01/test.py
@@ -1,1 +1,31 @@
-print("I'm so hungry..")
+import sys
+
+def gcd(a, b):
+    case = [a,b]
+    a = max(case)
+    b = min(case)
+    while(b != 0):
+        r = a % b
+        a = b
+        b = r
+    return a
+
+N = int(sys.stdin.readline())
+list_N = []
+for i in range(N):
+    list_N.append(int(sys.stdin.readline()))
+
+list_dis = []
+for j in range(len(list_N) - 1):
+    list_dis.append(list_N[j + 1] - list_N[j])
+
+list_dis.sort()
+r = list_dis[0]
+for k in range(len(list_dis)):
+    r = gcd(r, list_dis[k])
+
+count = 0
+for p in range(len(list_dis)):
+    count += list_dis[p] // r - 1
+
+print(int(count))
--- day02/03.py
+++ day02/03.py
@@ -1,6 +1,13 @@
-n = 0
-while(True):
-    print(n)
-    n += 1
-    if n == 10:
-        break
+money = 5,000,000
+print(money)
+print(type(money)) #tuple
+money=5_000_000
+print(money)
+print(type(money)) #int
+print()
+
+base_num = int(input('Enter base number: '))
+expoent_num = int(input('Enter expoent number: '))
+print(f'result : {base_num ** expoent_num}')
+print("result : {}".format(base_num ** expoent_num))
+print("result : %d " % (base_num ** expoent_num))
