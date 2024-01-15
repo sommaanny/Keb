053ec04b9a46cfaee6d03885649f69ba507e167f
sommaanny <a000626@naver.com>
2024-01-15 14:29:44 +0900

    day02

--- day02/03.py
+++ day02/03.py
@@ -11,3 +11,12 @@ expoent_num = int(input('Enter expoent number: '))
 print(f'result : {base_num ** expoent_num}')
 print("result : {}".format(base_num ** expoent_num))
 print("result : %d " % (base_num ** expoent_num))
+
+print(divmod(9, 5))
+print(ord('a'))
+
+def temperature(temp):
+    result = (temp - 32) * 5 / 9
+    return result
+print("{:.4f}".format(temperature(100))) #화씨 -> 섮씨
+print(f'{temperature(100):.4f}')
