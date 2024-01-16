bb60f704b914b8b6dc7d66f7d9d216eaa461fda3
sommaanny <a000626@naver.com>
2024-01-16 11:04:59 +0900

    day03

--- day01/test.py
+++ day01/test.py
@@ -4,16 +4,10 @@ N = int(sys.stdin.readline())
 stack_x = list()
 
 for i in range(N):
-    n = int(sys.stdin.readline())
-    if n == 1:
-        x = int(sys.stdin.readline())
-        stack_x.append(x)
-        print(stack_x)
-    elif n == 2:
-        pass
-    elif n == 3:
-        pass
-    elif n == 4:
-        pass
-    elif n == 5:
-        pass
+    n = int(sys.stdin.readline().rstrip())
+    if n != 0:
+        stack_x.append(n)
+    else:
+        stack_x.pop()
+
+print(sum(stack_x))
