8ce466fda82ca51d9a2fd9dc5ce60bd1ec829a6f
sommaanny <a000626@naver.com>
2024-01-16 11:05:38 +0900

    day03

---
+++ day03/01.py
@@ -0,0 +1,11 @@
+test = "Hello"
+print(test)
+test = test.replace('H', 'K')
+print(test)
+test = 'H' + test[1:]
+print(test)
+print()
+test2 = "AAABBBCCC"
+print(test2)
+test2 = test2.replace('A', 'K', 2)
+print(test2)
