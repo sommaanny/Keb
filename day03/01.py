83ec96efe4482c1595cc52b5442872f2b1f50a6e
sommaanny <a000626@naver.com>
2024-01-16 11:39:39 +0900

    day03

--- day03/01.py
+++ day03/01.py
@@ -4,8 +4,15 @@ test = test.replace('H', 'K')
 print(test)
 test = 'H' + test[1:]
 print(test)
+
 print()
+
 test2 = "AAABBBCCC"
 print(test2)
 test2 = test2.replace('A', 'K', 2)
 print(test2)
+
+test3 = "What! the...!!?"
+print(test3.strip(".!?"))
+print(test3.replace('!', '').replace('.', '').replace('?', ''))
+
