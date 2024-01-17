db8c9249d013343a947aab4a1beee2343c923c72
sommaanny <a000626@naver.com>
2024-01-17 15:44:35 +0900

    day04

--- day04/01.py
+++ day04/01.py
@@ -1,3 +1,5 @@
+import copy
+
 list_a = [1, 2, 3]
 list_b = list_a
 list_c = list_a.copy()
@@ -11,6 +13,21 @@ print()
 for a, b in zip(list_a, list_b):
     print(a, b)
 
-dic_a = {1: 'a', 2: 'b'}
+dic_a = {'1': 'a', '2': 'b'}
 for key, value in dic_a.items():
     print(key, ":", value)
+
+list_test = [i for i in range(1, 11) if i % 2 == 0]
+print(list_test)
+
+a = 4
+b = copy.deepcopy(a)
+print(a, b)
+a = 3
+print(a, b)
+print()
+
+dic_b = dic_a
+dic_c = dic_a.copy()
+dic_d = copy.deepcopy(dic_a)
+print(dic_a, dic_b, dic_c, dic_d)
