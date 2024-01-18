a0bde3e5182d96e1e6fe4148b8b4bcb2c186d01a
sommaanny <a000626@naver.com>
2024-01-18 10:17:04 +0900

    day05

--- day05/01.py
+++ day05/01.py
@@ -1,3 +1,9 @@
-A = {1, 2, 3, 4}
-for i in A:
-    print(i)
+word = "INHA"
+test ={}
+test2 = {l : word.count(l) for l in word}
+for i in word:
+    test[i] = word.count(i)
+
+
+print(test)
+print(test2)
