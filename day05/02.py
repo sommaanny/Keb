eb2ee1c7921a25954ad1d7bea406c3f84c14f033
sommaanny <a000626@naver.com>
2024-01-18 14:18:12 +0900

    day05

--- day05/02.py
+++ day05/02.py
@@ -1,8 +1,19 @@
 def print_add(*args):
-    print(*args ** 2)
+    print(*args)
 
 def add_args(func, *args):
     func(*args)
 
 add_args(print_add, 1, 2, 3, 4, 5)
 
+def knights2(saying):
+    def inner2():
+        return "We are the knights who say: '%s'" % saying
+    return inner2
+
+a = knights2('Duck')
+b = knights2('Hasenpfeffer')
+print(a)
+print(b)
+print(a())
+print(b())
